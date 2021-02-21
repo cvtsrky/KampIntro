45335cde6ffb278dfa086d5722455feea836fd3c
Cevat Sarıkaya <cvtsrky38@gmail.com>
2021-02-21 11:42:58 +0300

    Kamp 4. Gün

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //Constructor // Bu class ı bir yerde new lersek constructorhemen çalışır.
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
